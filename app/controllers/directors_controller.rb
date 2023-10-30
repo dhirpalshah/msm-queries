d7620216e9d060f86a6532a701439bf45e828578
dhirpalshah <86183980+dhirpalshah@users.noreply.github.com>
2023-10-30 23:29:37 +0000

    just need to do oldest and youngest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,12 +5,16 @@ class DirectorsController < ApplicationController
 
   def show
     director_id = params["director_id"]
-
     @director = Director.where({:id => director_id}).at(0)
-    
-    
     render({:template => "director_templates/details"})
+  end
 
+  def youngest
+    render({:template => "director_templates/youngest"})
+  end
+
+  def eldest
+    render({:template => "director_templates/eldest"})
   end
   
 end
