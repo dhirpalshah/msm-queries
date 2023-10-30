40984a155efd6909c884568925ab7f7eada91aa6
dhirpalshah <86183980+dhirpalshah@users.noreply.github.com>
2023-10-30 22:41:47 +0000

    40percentdone

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,5 +2,15 @@ class DirectorsController < ApplicationController
   def index
     render({:template => "director_templates/list"})
   end
-end
 
+  def show
+    director_id = params["director_id"]
+
+    @director = Director.where({:id => director_id}).at(0)
+    
+    
+    render({:template => "director_templates/details"})
+
+  end
+  
+end
